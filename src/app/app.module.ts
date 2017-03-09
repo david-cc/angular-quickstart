312f7085da0601c69b4d10f191030b7a082e61ce
david-cc <viddouille.cc@gmail.com>
2017-03-09 16:05:45 +0100

    QuickStart Angular commit part 6

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule }     from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service'
+
+import { AppRoutingModule }     from './app-routing.module';
+import { AppComponent }         from './app.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroService }          from './hero.service';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent ],
+  bootstrap: [ AppComponent ],
+  providers: [ HeroService ]
+})
+
+export class AppModule { }
